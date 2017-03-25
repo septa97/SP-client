7826f88d2f4ef3fd242ace5efc8a3477804af733
septa97 <jancharles.adona@gmail.com>
2017-03-25 16:01:54 +0800

    Added trainer and course template for routing

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
-import GMapsView from './components/GMaps.vue'
-import VisualizerView from './components/Visualizer.vue'
+import GMapsView        from './components/GMaps.vue'
+import TrainerView      from './components/Trainer.vue'
+import VisualizerView   from './components/Visualizer.vue'
+import CourseView       from './components/Course.vue'
 
 export default [
     { path: '/gmaps', component: GMapsView },
-    { path: '/visualizer', component: VisualizerView }
+    { path: '/trainer', component: TrainerView },
+    { path: '/visualizer', component: VisualizerView },
+    { path: '/course/:id', component: CourseView }
 ]
