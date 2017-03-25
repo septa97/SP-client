4dbb3abd4babcaaf8e05bb8ff0982af40b995644
septa97 <jancharles.adona@gmail.com>
2017-03-25 16:03:48 +0800

    Renamed api_routes.js to apiRoutes.js

---
+++ src/apiRoutes.js
@@ -0,0 +1,12 @@
+const rethinkdbBaseURL = 'http://localhost:5000/api/v1/rethinkdb'
+const courseraBaseURL = 'http://localhost:5000/api/v1/coursera'
+const classifierBaseURL = 'http://localhost:5000/api/v1/classifier'
+const visualizerBaseURL = 'http://localhost:5000/api/v1/visualizer'
+
+
+export default {
+    rethinkdbBaseURL,
+    courseraBaseURL,
+    classifierBaseURL,
+    visualizerBaseURL
+}
