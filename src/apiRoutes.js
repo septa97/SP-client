4a8d1db5ee35ba2e64399c7ec3a4558b0f56acdc
septa97 <jancharles.adona@gmail.com>
2017-03-26 21:56:31 +0800

    Added baseURL for dimensionality reduction routes

--- src/apiRoutes.js
+++ src/apiRoutes.js
@@ -2,11 +2,13 @@ const rethinkdbBaseURL = 'http://localhost:5000/api/v1/rethinkdb'
 const courseraBaseURL = 'http://localhost:5000/api/v1/coursera'
 const classifierBaseURL = 'http://localhost:5000/api/v1/classifier'
 const visualizerBaseURL = 'http://localhost:5000/api/v1/visualizer'
+const dimensionalityReductionBaseURL = 'http://localhost:5000/api/v1/dimensionality-reduction'
 
 
 export default {
     rethinkdbBaseURL,
     courseraBaseURL,
     classifierBaseURL,
-    visualizerBaseURL
+    visualizerBaseURL,
+    dimensionalityReductionBaseURL
 }
