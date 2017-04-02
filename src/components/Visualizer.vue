6f3c979080f62679cff093f73c2b08b48ef2d2ee
septa97 <jancharles.adona@gmail.com>
2017-04-02 21:42:25 +0800

    Modified Visualizer.vue

--- src/components/Visualizer.vue
+++ src/components/Visualizer.vue
@@ -1,15 +1,49 @@
 <template>
-    <div id="visualizer">
+    <div id="main">
+        <div id="options">
+            <div class="row">
+                <div class="input-field col s6">
+                    <input id="data-size" type="text" v-model.trim.number="dataSize">
+                    <label for="data-size">Data Size</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col s6">
+                    <label for="min-df">Minimum Document Frequency</label>
+                    <input id="min-df" type="range" min="5" max="20" v-model.trim.number="minDF">
+                    <output for="min-df">{{ minDF }}</output>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-field col s6">
+                    <button class="btn waves-effect waves-light" type="submit" @click="getAllInformations">
+                        Visualize
+                    </button>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="progress" v-if="retrievingData">
+                    <div class="indeterminate"></div>
+                </div>
+            </div>
+        </div>
+
         <div id="charts">
             <h5 v-show="PCA2D">Principal Component Analysis (2D)</h5>
             <div id="PCA-2D">
             </div>
+
             <h5 v-show="PCA3D">Principal Component Analysis (3D)</h5>
             <div id="PCA-3D">
             </div>
+
             <h5 v-show="tSNE2D">t-distributed Stochastic Neighbor Embedding (2D)</h5>
             <div id="tSNE-2D">
             </div>
+
             <h5 v-show="tSNE3D">t-distributed Stochastic Neighbor Embedding (3D)</h5>
             <div id="tSNE-3D">
             </div>
@@ -22,25 +56,30 @@
 
     import apiRoutes from './../apiRoutes'
     import axios from 'axios'
+    import async from 'async'
     import _ from 'lodash'
 
     export default {
         data() {
             return {
+                dataSize: 1000,
+                minDF: 5,
                 PCA2D: false,
                 PCA3D: false,
                 tSNE2D: false,
-                tSNE3D: false
+                tSNE3D: false,
+                retrievingData: false
             }
         },
         mounted() {
-            this.getAllInformations()
         },
         methods: {
             getAllInformations() {
-                const urls = ['/PCA/2', '/PCA/3', '/tSNE/2', '/tSNE/3']
+                this.retrievingData = true
 
-                _.forEach(urls, url => {
+                const urls = [`/PCA/${this.minDF}/${this.dataSize}/2`, `/PCA/${this.minDF}/${this.dataSize}/3`, `/tSNE/${this.minDF}/${this.dataSize}/2`, `/tSNE/${this.minDF}/${this.dataSize}/3`]
+
+                async.each(urls, (url, callback) => {
                     const config = {
                         method: 'get',
                         baseURL: apiRoutes.dimensionalityReductionBaseURL,
@@ -57,6 +96,8 @@
 
                                 trace.x = response.data[type].X
                                 trace.y = response.data[type].y
+                                trace.text = response.data[type].reviews
+
                                 if (url[url.length-1] === '3') {
                                     trace.z = response.data[type].z
                                     trace.type = 'scatter3d'
@@ -73,25 +114,32 @@
 
                             let div
                             switch (url) {
-                                case '/PCA/2':  div = 'PCA-2D'
-                                                this.PCA2D = true
-                                                break
-                                case '/PCA/3':  div = 'PCA-3D'
-                                                this.PCA3D = true
-                                                break
-                                case '/tSNE/2': div = 'tSNE-2D'
-                                                this.tSNE2D = true
-                                                break
-                                case '/tSNE/3': div = 'tSNE-3D'
-                                                this.tSNE3D = true
-                                                break
+                                case `/PCA/${this.minDF}/${this.dataSize}/2`:   div = 'PCA-2D'
+                                                                                this.PCA2D = true
+                                                                                break
+                                case `/PCA/${this.minDF}/${this.dataSize}/3`:   div = 'PCA-3D'
+                                                                                this.PCA3D = true
+                                                                                break
+                                case `/tSNE/${this.minDF}/${this.dataSize}/2`:  div = 'tSNE-2D'
+                                                                                this.tSNE2D = true
+                                                                                break
+                                case `/tSNE/${this.minDF}/${this.dataSize}/3`:  div = 'tSNE-3D'
+                                                                                this.tSNE3D = true
+                                                                                break
                             }
 
                             Plotly.newPlot(div, data)
+                            callback()
                         })
                         .catch(error => {
-                            throw new Error(error)
+                            callback(error)
                         })
+                }, error => {
+                    if (error) {
+                        throw new Error(error)
+                    }
+
+                    this.retrievingData = false
                 })
             }
         }
