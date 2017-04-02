b826b1cd6b275bc222c1190b31354da0dfc2108f
septa97 <jancharles.adona@gmail.com>
2017-04-02 21:44:34 +0800

    Modified src/routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
 import GMapsView         from './components/GMaps.vue'
-import PreprocessorView  from './components/Preprocessor.vue'
 import TrainerView       from './components/Trainer.vue'
 import VisualizerView    from './components/Visualizer.vue'
 import CourseView        from './components/Course.vue'
@@ -8,7 +7,6 @@ import tSNEView          from './components/tSNE.vue'
 export default [
     { path: '/gmaps',           component: GMapsView },
     { path: '/trainer',         component: TrainerView },
-    { path: '/preprocessor',    component: PreprocessorView },
     { path: '/visualizer',      component: VisualizerView },
     { path: '/course/:slug',    component: CourseView },
     { path: '/tSNE',            component: tSNEView }
